64ff88c7d53da02332c4cd2e604a51ad3a83dfd9
ikurowski <igorkurowski@gmail.com>
2022-12-01 20:28:45 +0100

    add global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import styles from './styles';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: ${styles.font.main};
+}
+
+body {
+  margin: 0;
+  background-color:
+  ${styles.color.primary};
+}
+`;
+
+export default GlobalStyle;
