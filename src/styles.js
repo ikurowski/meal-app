30694fee50e05b3834fe61173234b9757e462ffe
ikurowski <igorkurowski@gmail.com>
2022-12-01 20:29:55 +0100

    add colors variables

---
+++ src/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  color: {
+    primary: '#355070',
+    primaryDark: '#315070',
+    secondary: '#e56b6f',
+    secondaryDark: '#b56576',
+    textLight: '#fffc8b',
+    light: '#f4f1de',
+  },
+  font: {
+    main: 'sans-serif',
+  },
+};
+export default styles;
