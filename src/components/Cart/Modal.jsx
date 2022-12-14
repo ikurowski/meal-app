af96978794dbee479edc8f52ac456b3d44bef054
ikurowski <igorkurowski@gmail.com>
2022-12-14 12:09:55 +0100

    add Modal component

---
+++ src/components/Cart/Modal.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable styled-components-a11y/click-events-have-key-events */
+/* eslint-disable styled-components-a11y/no-static-element-interactions */
+import React, { useCallback } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+// components
+import Cart from './Cart';
+
+const portalElement = document.getElementById('overlay');
+
+function Backdrop({ hideModal }) {
+  const backdropClickHandler = useCallback(
+    (e) => {
+      if (e.target === e.currentTarget) {
+        hideModal();
+      }
+    },
+    [hideModal],
+  );
+
+  return (
+    <BackdropStyled onClick={backdropClickHandler}>
+      <Cart hideModal={hideModal} />
+    </BackdropStyled>
+  );
+}
+
+export default function Modal({ hideModal }) {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop hideModal={hideModal} />, portalElement)}
+    </>
+  );
+}
+
+const BackdropStyled = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
