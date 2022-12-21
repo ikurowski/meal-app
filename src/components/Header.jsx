5773457d4c2f9e3d92d720be617eb2e5d666b64d
ikurowski <igorkurowski@gmail.com>
2022-12-21 18:31:14 +0100

    Button animation done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useContext, useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 import styles from '../styles';
 import { pixelToViewportWidth } from '../utils/utils';
+
+// context
+import CartContext from '../store/CartContext';
+
+// components
 import Button from './Button';
 
+const bump = keyframes`
+   0% {
+      transform: scale(1);
+    }
+    10% {
+      transform: scale(0.9);
+    }
+    30% {
+      transform: scale(1.1);
+    }
+    50% {
+      transform: scale(1.3);
+    }
+    100% {
+      transform: scale(1);
+    }
+`;
+
+const bumpAnimation = css`
+  animation: ${bump} 300ms ease-out;
+`;
+
 export default function Header({ showModal }) {
-  const itemsInCart = <Badge>1</Badge>;
+  const [animation, setAnimation] = useState(false);
+  const cartContext = useContext(CartContext);
+  const numberOfMealsInCart = cartContext.items.reduce(
+    (acc, item) => acc + item.amount,
+    0,
+  );
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return undefined;
+    }
+    setAnimation(true);
+
+    const timer = setTimeout(() => {
+      setAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
+  const itemsInCart = <Badge>{numberOfMealsInCart}</Badge>;
+
   return (
     <HeaderStyled>
       <Logo href=".">MealsUp</Logo>
-      <Button onClick={showModal} ariaLabel="your cart">
-        Your Cart {itemsInCart || null}
+      <Button
+        onClick={showModal}
+        ariaLabel="your cart"
+        moresStyles={animation && bumpAnimation}
+      >
+        Your Cart {cartContext.items.length ? itemsInCart : null}
       </Button>
     </HeaderStyled>
   );
